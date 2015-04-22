9d3efec9e06ba1ba686946605eb08a66e77833c0
Jonathan Matthey <mattheyj@gmail.com>
2015-04-21 20:52:05 -0400

    add ajax load

--- js/main.js
+++ js/main.js
@@ -26,12 +26,48 @@ $(document).ready(function(){
 
   $(".thumb").click(function(e){
     var i = $(e.target).index();
-    $(".center-col .main-img:visible").removeClass('show');
+    $(".center-col .main-img.show").removeClass('show');
     $(".center-col .main-img").eq(i).addClass('show');
   });
 
   setTimeout(function(){
-    $(".splash").fadeOut(1000);
-  },1000);
+    $(".splash").fadeOut(500);
+  }, 2000);
 
+  $(".splash").click(function(){
+    $(".splash").fadeOut(500);
+  });
+
+  $("body").keydown(function(e) {
+    if(e.keyCode == 37) { // left
+      if($(".center-col .main-img.show").index() > 0){
+        $(".center-col .main-img.show").removeClass('show').prev('.main-img').addClass('show');
+      }
+    }
+    else if(e.keyCode == 39) { // right
+      if($(".center-col .main-img.show").index() < $(".center-col .main-img").length-1){
+        $(".center-col .main-img.show").removeClass('show').next('.main-img').addClass('show');
+      }
+    }
+  });
+
+  // ajax content in
+  $(".nav a").click(function(e){
+    e.preventDefault();
+    var targetPage = $(e.currentTarget).attr('href');
+    if (window.location.href.indexOf(targetPage) == -1){
+      $("nav").removeClass('open');
+      $.ajax({
+         url: targetPage,
+         data: {},
+         success: function (data) {
+            $("#main-content").fadeOut(400,function(){
+              $("#main-content").html($(data).find("#main-content"));
+              $("#main-content").fadeIn(600);
+            })
+         },
+         dataType: 'html'
+      });
+    }
+  });
 });
